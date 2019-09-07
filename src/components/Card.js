f59349f15553f34b553da08bb879a3f1c76f4fa8
ginakui <developerkui@outlook.com>
2019-09-07 18:00:03 -0400

    deleted Scroll component, delete some unrelated files, adjust the app structure a little

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = ({ name, email, id }) => {
+const Card = ({ name, website, id }) => {
   return (
     <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
-      <img alt='robots' src={`https://robohash.org/${id}?set=set4;size=200x200`} />
+      <img alt='robots' src={`https://robohash.org/${id}?set=set4;size=160x160`} />
       <div>
         <h2>{name}</h2>
-        <p>{email}</p>
+        <p>{website}</p>
       </div>
     </div>
   );
